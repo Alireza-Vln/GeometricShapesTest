b3a5e1b1c3e1305dad12f76a7b48006c79e5c622
Alireza-Vln <alirezavaladan74@gmail.com>
2024-02-23 13:04:10 +0330

    creat-geometric-shapees

---
+++ GeometricShapes/Perimeter.cs
@@ -0,0 +1,31 @@
+﻿namespace GeometricShapesTest
+{
+   public class Perimeter
+    {
+        public double CirclePerimeter(double pi, double raduis)
+        {
+
+            var perimeter=2*pi*raduis;
+            return perimeter;
+        }
+
+        public double RectanglePrimeter(double length, double width)
+        {
+         var perimeter=2*(length+width);
+            return perimeter;
+        }
+
+        public double Squareperimeter(double side)
+        {
+            var peremeter = 4 * side;
+            return peremeter;
+
+        }
+
+        public double TrianglePremeter(double height, double side, double baase)
+        {
+            var primeter=side+baase+height;
+            return primeter;
+        }
+    }
+}
---
+++ GeometricShapesTest/PerimeterTest.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapesTest
+{
+    public class PerimeterTest
+    {
+        [Fact]
+
+        public void Add_perimeter_circle_with_pi_and_raduis()
+        {
+            var pi = 3.14;
+            var raduis = 100;
+            var expected = 628;
+            var perimeter = new Perimeter();
+
+
+            var actual = perimeter.CirclePerimeter(pi, raduis);
+
+            actual.Should().Be(expected);
+
+
+        }
+        [Fact]
+
+        public void Add_perimeter_Rectangle_with_length_and_width()
+        {
+            var length = 10;
+            var width= 10;
+            var expected=40;
+            var perimeter = new Perimeter();
+
+
+            var actual=perimeter.RectanglePrimeter(length, width);
+
+
+            actual.Should().Be(expected);
+        }
+        [Fact]
+        public void Add_perimeter_square_with_side()
+        {
+            var side = 10;
+            var expected = 40;
+            var perimeter= new Perimeter();
+
+            var actual = perimeter.Squareperimeter(side);
+
+            actual.Should().Be(expected);
+
+
+
+        }
+        [Fact]
+        public void Add_perimeter_triangle_with_base_and_height_and_side()
+        {
+            var height = 10;
+            var side = 10;
+            var Base = 10;
+            var expected = 30;
+            var perimeter = new Perimeter();
+
+
+            var actual=perimeter.TrianglePremeter(height, side, Base);
+
+
+            actual.Should().Be(expected);   
+            
+
+            
+
+        }
+
+    }
+}
